ac35284eb2de13816ff38fb0de9f7d4c425d03a9
Guilherme Dadald <guigasparotto@gmail.com>
2018-08-08 22:01:33 +0100

    Correcting HomePageTest class

--- src/main/java/com/automationpractice/pages/HomePage.java
+++ src/main/java/com/automationpractice/pages/HomePage.java
@@ -1,33 +1,87 @@
 package com.automationpractice.pages;
 
 import com.automationpractice.base.TestBase;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends TestBase {
 
-    private WebDriver driver;
+    @FindBy(xpath = "//img[@class='img-responsive']")
+    WebElement banner;
 
-    @FindBy(xpath = "//a[@loginPageTitle='Log in to your customer account']")
-    WebElement signInButton;
+    @FindBy(xpath = "//img[@class='logo img-responsive']")
+    private WebElement pageLogo;
+
+    @FindBy(xpath = "//a[@title='Contact Us']")
+    WebElement contactUsLink;
+
+    @FindBy(xpath = "//a[@title='Log in to your customer account']")
+    WebElement signInLink;
+
+    @FindBy(id = "search_query_top")
+    WebElement searchField;
+
+    @FindBy(name = "submit_search")
+    WebElement submitSearch;
+
+    @FindBy(xpath = "//a[@title='View my shopping cart']")
+    WebElement shoppingCartLink;
+
+    @FindBy(xpath = "//a[@title='Women']")
+    WebElement womenLink;
+
+    @FindBy(xpath = "//body[@id='index']/div[@id='page']/div[@class='header-container']" +
+            "/header[@id='header']/div/div[@class='container']/div[@class='row']/div[@id='block_top_menu']" +
+            "/ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li[2]/a[1]")
+    WebElement dressesLink;
+
+    @FindBy(xpath = "//body[@id='index']/div[@id='page']/div[@class='header-container']" +
+            "/header[@id='header']/div/div[@class='container']/div[@class='row']/div[@id='block_top_menu']" +
+            "/ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li[3]/a[1]")
+    WebElement tshirtsLink;
 
     public HomePage() {
         // Initialises the web elements
         PageFactory.initElements(driver, this);
     }
 
-    public void clickSignIn() {
-        driver.findElement(By.xpath("//a[@loginPageTitle='Log in to your customer account']")).click();
+    public String getHomePageTitle() {
+        return this.getHomePageTitle();
     }
 
     // Clicks in the Sign In button and navigates to the login page
     public LoginPage clickSignInLink() {
-        signInButton.click();
+        signInLink.click();
         return new LoginPage();
     }
+
+    public SearchPage testSearch(String searchItem) {
+        searchField.sendKeys(searchItem);
+        submitSearch.click();
+        return new SearchPage();
+    }
+
+    public ContactUsPage clickContactUsLink() {
+        contactUsLink.click();
+        return new ContactUsPage();
+    }
+
+    public WomenPage clickWomenLink() {
+        womenLink.click();
+        return new WomenPage();
+    }
+
+    public DressesPage clickDressesLink() {
+        dressesLink.click();
+        return new DressesPage();
+    }
+
+    public TshirtsPage clickTshirtsLink() {
+        tshirtsLink.click();
+        return new TshirtsPage();
+    }
+
 
 
 }
