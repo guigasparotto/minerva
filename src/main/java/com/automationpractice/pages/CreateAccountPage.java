d763c5472aa11dada88682ca2e9e915d8c9752d0
Guilherme Dadald <guigasparotto@gmail.com>
2018-08-14 23:27:49 +0100

    Added WebElements for CreateAccountPage

--- src/main/java/com/automationpractice/pages/CreateAccountPage.java
+++ src/main/java/com/automationpractice/pages/CreateAccountPage.java
@@ -3,101 +3,137 @@ package com.automationpractice.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class CreateAccountPage {
 
     WebDriver driver;
 
     @FindBy(xpath = "//h1[@class='page-heading']")
-    WebElement pageHeading;
+    private WebElement pageHeading;
 
     @FindBy(xpath = "//h3[contains(text(),'Your personal information')]")
-    WebElement pageSubheading;
+    private WebElement pageSubheading;
 
     @FindBy(id="id_gender1")
-    WebElement maleRadioButton;
+    private WebElement maleRadioButton;
 
     @FindBy(id="id_gender2")
-    WebElement femaleRadioButton;
+    private WebElement femaleRadioButton;
 
     @FindBy(id="customer_firstname")
-    WebElement firstNameField;
+    private WebElement firstNameField;
 
     @FindBy(id="customer_lastname")
-    WebElement lastNameField;
+    private WebElement lastNameField;
 
     @FindBy(id="email")
-    WebElement emailField;
+    private WebElement emailField;
 
     @FindBy(id="passwd")
-    WebElement passwordField;
+    private WebElement passwordField;
 
     @FindBy(id="days")
-    WebElement daysDropDown;
+    private WebElement daysDropDown;
 
     @FindBy(id="months")
-    WebElement monthsDropDown;
+    private WebElement monthsDropDown;
 
     @FindBy(id="years")
-    WebElement yearsDropDown;
+    private WebElement yearsDropDown;
 
     @FindBy(id="newsletter")
-    WebElement newsletterCheckbox;
+    private WebElement newsletterCheckbox;
 
     @FindBy(id="optin")
-    WebElement receiveOffersCheckbox;
+    private WebElement receiveOffersCheckbox;
 
     @FindBy(id="years")
-    WebElement years;
+    private WebElement years;
 
     @FindBy(id="firstname")
-    WebElement addressFirstName;
+    private WebElement addressFirstName;
 
     @FindBy(id="lastname")
-    WebElement addressLastName;
+    private WebElement addressLastName;
 
     @FindBy(id="company")
-    WebElement companyField;
+    private WebElement companyField;
 
     @FindBy(id="address1")
-    WebElement addressField;
+    private WebElement addressField;
 
     @FindBy(id="address2")
-    WebElement addressCompField;
+    private WebElement addressCompField;
 
     @FindBy(id="city")
-    WebElement cityField;
+    private WebElement cityField;
 
     @FindBy(id="id_state")
-    WebElement stateDropDown;
+    private WebElement stateDropDown;
 
     @FindBy(id="postcode")
-    WebElement postCodeField;
+    private WebElement postCodeField;
 
     @FindBy(id="id_country")
-    WebElement countryDropDown;
+    private WebElement countryDropDown;
 
     @FindBy(id="other")
-    WebElement additionalInfoField;
+    private WebElement additionalInfoField;
 
     @FindBy(id="phone")
-    WebElement homePhoneField;
+    private WebElement homePhoneField;
 
     @FindBy(id="phone_mobile")
-    WebElement mobilePhoneField;
+    private WebElement mobilePhoneField;
 
     @FindBy(id="alias")
-    WebElement addressAliasField;
+    private WebElement addressAliasField;
 
-    @FindBy(css = "body.authentication.hide-left-column.hide-right-column.lang_en:nth-child(2) " +
-            "div.columns-container div.container div.row:nth-child(3) div.center_column.col-xs-12.col-sm-12 " +
-            "form.std.box div.submit.clearfix:nth-child(4) button.btn.btn-default.button.button-medium:" +
-            "nth-child(4) > span:nth-child(1)")
-    WebElement registerButton;
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/form[1]/div[4]/button[1]/span[1]")
+    private WebElement registerButton;
 
     public CreateAccountPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    public void setGenderMale() {
+        maleRadioButton.click();
+    }
+
+    public void setGenderFemale() {
+        femaleRadioButton.click();
+    }
+
+    public void setFirstName(String firstName) {
+        firstNameField.sendKeys(firstName);
+    }
+
+    public void setlastName(String lastName) {
+        lastNameField.sendKeys(lastName);
+    }
+
+    public void setEmail(String email) {
+        emailField.sendKeys(email);
+    }
+
+    public void setPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void setBirthDay(String day) {
+        Select select = new Select(daysDropDown);
+        select.selectByVisibleText(day);
+    }
+
+    public void setBirthMonth(String month) {
+        Select select = new Select(monthsDropDown);
+        select.selectByVisibleText(month);
+    }
+
+    public void setBirthYear(String year) {
+        Select select = new Select(yearsDropDown);
+        select.selectByVisibleText(year);
+    }
 
 }
