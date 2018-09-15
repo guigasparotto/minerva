341a9385db93fcc2f8e0feb121638b2c36ab0925
Guilherme Dadald <guigasparotto@gmail.com>
2018-09-15 18:54:33 +0100

    Added event listeners to generate better logging

--- src/main/java/com/automationpractice/util/TestUtil.java
+++ src/main/java/com/automationpractice/util/TestUtil.java
@@ -1,16 +1,21 @@
 package com.automationpractice.util;
 
+import com.automationpractice.base.TestBase;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Random;
 
-public class TestUtil {
+public class TestUtil extends TestBase {
 
     public static long PAGE_LOAD_TIMEOUT = 20;
     public static long IMPLICIT_WAIT = 10;
@@ -58,4 +63,12 @@ public class TestUtil {
         return data;
     }
 
+    public static void takeScreenshotAtEndOfTest() throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String currentDir = System.getProperty("user.dir");
+
+        // From dependency commons-io
+        FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+
+    }
 }
