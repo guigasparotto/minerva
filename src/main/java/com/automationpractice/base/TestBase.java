bb9ed6b0fb87e80fe33e558462b155a16427551f
Guilherme Dadald <guigasparotto@gmail.com>
2018-09-17 22:53:38 +0100

    minor changes

--- src/main/java/com/automationpractice/base/TestBase.java
+++ src/main/java/com/automationpractice/base/TestBase.java
@@ -44,7 +44,7 @@ public class TestBase {
         e_driver.register(eventListener);
         driver = e_driver;
 
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
